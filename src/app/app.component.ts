7962699ed2f5e2542e9ce719ec1f356fbf5edaf7
Przemyslaw Warchol <warcholprzemo@gmail.com>
2021-02-14 16:13:01 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lets-react-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Its-Angulars-Fault';
+}
